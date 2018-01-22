9d0e347f00a45082e0554ee1a86e9641e189c36d
Kirill Rysin <kirill.rysin@busnet.co.il>
2018-01-22 13:06:09 +0200

    suites parse error try/catch

--- writer.js
+++ writer.js
@@ -6,7 +6,11 @@ var fs = require('fs-extra'),
 
 module.exports = {
     writeSuite: function(targetDir, suites) {
-        fs.outputFileSync(path.join(targetDir, uuid.v4() + '-testsuite.xml'), xml.parse('ns2:test-suite', suites.toXML()));
+        try{
+            fs.outputFileSync(path.join(targetDir, uuid.v4() + '-testsuite.xml'), xml.parse('ns2:test-suite', suites.toXML()));
+        }catch(err){
+            console.log(err)
+        }
     },
     writeBuffer: function(targetDir, buffer, ext) {
         var fileName = uuid.v4() + '-attachment.' + ext;
